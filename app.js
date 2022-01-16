c8dd1969bbbd5010fd7f8b4804429f89ad3c0fdc
tdddt <doto1116@ewhain.net>
2022-01-16 17:00:56 +0900

    login

---
+++ app.js
@@ -0,0 +1,36 @@
+/*const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+*/
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+// get and show the username
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`; //"Hello "+username
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit); 
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);
+}

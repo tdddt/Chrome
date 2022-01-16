feb1111e85683c889ef765bd479032939c8d3c9e
tdddt <doto1116@ewhain.net>
2022-01-16 20:26:34 +0900

    feat:deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -14,15 +14,18 @@ function saveToDos() {
 }
 
 function deleteToDo(event) {
-    const li = event.target.parentElement;
+    const li = event.target.parentElement; 
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
     li.remove();
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
 
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
 
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -37,8 +40,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = ""; 
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newToDoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
